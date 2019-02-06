ac6e9dcd6fc231a48e4ab66d2977afa7ff20c7f3
Daniel <daniel.garibaldi@coosto.com>
2019-02-06 11:57:08 +0100

    updated glide to version 4.8

---
+++ matisse/src/main/java/com/zhihu/matisse/engine/impl/MatisseGlideModule.kt
@@ -0,0 +1,7 @@
+package com.zhihu.matisse.engine.impl
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class MatisseGlideModule : AppGlideModule()
